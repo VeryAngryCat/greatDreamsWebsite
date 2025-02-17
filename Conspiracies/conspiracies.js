2ad5cf83a80892e194100a260da90b02ce2e4827
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-17 12:52:25 +0400

    Adjusted style of conspiracies

--- Conspiracies/conspiracies.js
+++ Conspiracies/conspiracies.js
@@ -9,11 +9,19 @@ function shuffleArray(array) {
 function randomiseLinks() {
     const container = document.getElementById('temp-link-storage');
     const links = Array.from(container.getElementsByTagName('a'));
-    const shuffled = shuffleArray(links);
-    container.innerHTML = '';
-    shuffled.forEach(link => {
-        container.appendChild(link);
+    return shuffleArray(links);
+}
+
+function distributeLinks(){
+    const links = randomiseLinks();
+    const linkBar = document.querySelector('.linkBar');
+    const linkMenu = document.querySelector('.sideLinkMenu');
+    linkBar.appendChild(links[0]);
+    linkBar.appendChild(links[1]);
+    
+    links.slice(2).forEach(link => {
+        linkMenu.appendChild(link);
     });
 }
 
-window.onload = randomiseLinks;
+window.onload = distributeLinks;
