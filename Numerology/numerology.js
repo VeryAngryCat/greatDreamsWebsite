2da03535445b6a4db32f830ea86fd2fe3f9ccc34
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-18 11:59:30 +0400

    Created basic structure for numerology, no design or links added

---
+++ Numerology/numerology.js
@@ -0,0 +1,24 @@
+function shuffleArray(array) {
+    for(let i = array.length-1;i>0;i--){
+        const j = Math.floor(Math.random()*(i+1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function randomiseLinks() {
+    const container = document.getElementById('temp-link-storage');
+    const links = Array.from(container.getElementsByTagName('a'));
+    return shuffleArray(links);
+}
+
+function distributeLinks(){
+    const links = randomiseLinks();
+    const linkMenu = document.querySelector('.linkMenu');
+    
+    links.forEach(link => {
+        linkMenu.appendChild(link);
+    });
+}
+
+window.onload = distributeLinks;
