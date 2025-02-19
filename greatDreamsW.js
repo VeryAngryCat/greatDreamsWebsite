7e8f28aa69df16bd5dbec21a39b725384f23b1c0
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-19 09:38:58 +0400

    Added menu to extraterrestrial

--- greatDreamsW.js
+++ greatDreamsW.js
@@ -1,3 +1,21 @@
 function preview() {
     //Code for previewing link content on all link pages.
 }
+
+function toggleMenu() {
+    const icon = document.querySelector('.icon');
+    const mLinks = document.querySelector('.menuLinks');
+    const styleDisp = getComputedStyle(mLinks).display;
+    if (icon.textContent === "☰") {
+        icon.innerHTML = '&#10005;';
+    } else {
+        icon.innerHTML = '&#x2630;';
+    }
+
+    if (styleDisp === 'flex') {
+        mLinks.style.display = 'none';
+    } else {
+        mLinks.style.display = 'flex';
+    }
+    icon.classList.toggle('open');
+}
