cbfedd8d18cdb44cf0946fe67ac1728015e0472d
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-19 10:47:24 +0400

    Added menu to transcripts

--- Numerology/numerology.js
+++ Numerology/numerology.js
@@ -20,21 +20,4 @@ function distributeLinks(){
         linkMenu.appendChild(link);
     });
 }
-function toggleMenu() {
-    const icon = document.querySelector('.icon');
-    const mLinks = document.querySelector('.menuLinks');
-    const styleDisp = getComputedStyle(mLinks).display;
-    if (icon.textContent === "☰") {
-        icon.innerHTML = '&#10005;';
-    } else {
-        icon.innerHTML = '&#x2630;';
-    }
-
-    if (styleDisp === 'flex') {
-        mLinks.style.display = 'none';
-    } else {
-        mLinks.style.display = 'flex';
-    }
-    icon.classList.toggle('open');
-}
 window.onload = distributeLinks;
