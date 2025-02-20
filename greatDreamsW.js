faea988986b1febd63004f9be6de24cbae23dd80
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-20 23:07:40 +0400

    Search bar working!! Just need to hide+position it)))

--- greatDreamsW.js
+++ greatDreamsW.js
@@ -1,5 +1,18 @@
-function preview() {
-    //Code for previewing link content on all link pages.
+function search() {
+    var input, list, ul, li, a, i, txtValue;
+    input = document.querySelector('.input');
+    list = input.value.toUpperCase();
+    ul = document.querySelector('.linkList');
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(list) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
 function toggleMenu() {
