8bd55c37bfceb29b19c3e0465334bdd2dc6f2fd5
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-02-20 23:53:02 +0400

    Yooo the search list is now hidden if the search bar isn't active

--- greatDreamsW.js
+++ greatDreamsW.js
@@ -18,6 +18,34 @@ function search() {
     }
 }
 
+// Event listener for when the user clicks on search bar
+document.addEventListener("DOMContentLoaded", function() {
+    // Gets input from search bar and list of links
+    const input = document.querySelector('.input');
+    const links = document.querySelector('.linkList');
+
+    // If the search bar is in 'focus', or active, the links are shown
+    input.addEventListener("focus", function() {
+        links.style.display = "block";
+    });
+
+    // If there is an event e of a click somewhere else, search list dissappears
+    document.addEventListener("click", function(e) {
+        if (!input.contains(e.target) && !links.contains(e.target)) {
+            links.style.display = "none";
+        }
+    });
+
+    // If the search bar is activated, and there is something shows relevant list, otherwise hides it
+    input.addEventListener("input", function() {
+        if (this.value != "") {
+            links.style.display = "block";
+        } else {
+            links.style.display = "none";
+        }
+    });
+});
+
 function toggleMenu() {
     const icon = document.querySelector('.icon');
     const mLinks = document.querySelector('.menuLinks');
